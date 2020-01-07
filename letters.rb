553dc288acd50d7ef7f26b5f6ba0e4de35044ff6
arielzapata <45351266+arielzapata@users.noreply.github.com>
2020-01-07 15:26:30 -0600

    Corrections of letter.rb

--- letters.rb
+++ letters.rb
@@ -1,6 +1,6 @@
 def consecutive_letters(letters)
-  (letters).each do |letters|
-    return unless ("a".."z").to_a.join.include? letters.downcase
+  letters.each do |letters|
+    return unless ("a".."z").to_a.join.include? (letters.downcase)
   end
   true
 end
