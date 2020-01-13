018061b57342ce1845c057c3c88de1984c52baa5
Ariel Zapata <ariel.zapata.j@gmail.com>
2020-01-13 17:57:51 -0600

    Script modified

--- models/similar_home.rb
+++ models/similar_home.rb
@@ -1,33 +1,32 @@
-def similar_homes
-  departaments_id = []
-  departaments_owner = []
-    
-  Owner.pluck(:id).each do |id_owner|
-    id_owner = id_owner.to_s
-    departaments = Home.pluck(:id).map { |own,home = Home.find(own)| own if id_owner == home.owner.id.to_s }
-    departaments_id.push(departaments)
-  end
+class SimilarHome
+  include Mongoid::Document
+  def similar_homes
+    home_owner = []
+    homes = []
+      
+    Owner.pluck(:id).each do |id_owner|
+      id_owner = id_owner
+      homes = Owner.includes(:homes).find(id_owner).homes
   
-  homes = departaments_id.each &:compact!   
- 
-  homes.each do |departments_group| 
-    departments_group.each do |department|
-      homies =  Home.find(department)
-        departments_group.each do |departament_slice|
-        homies2 =  Home.find(departament_slice)
-        if department != departament_slice && homies.total_amount == homies2.total_amount  
-          if homies.status == homies2.status && homies.location == homies2.location && homies.master_home_id.nil?
-            departaments_owner.push(homies.id.to_s)
-          end              
-        end  
+      homes.each do |home| #comparacion entre los depas del owner (actual vs iteraciones)
+        homies =  Home.find(home) 
+        homes.each do |home_slice|
+          current_home =  Home.find(home_slice) #Compara el home actual con todos
+          next unless home != home_slice
+          next unless homies.total_amount == current_home.total_amount
+          next unless homies.status == current_home.status 
+          next unless homies.location == current_home.location 
+          next unless homies.home_master_id.nil?
+            home_owner.push(homies.id.to_s)
+        end           
       end
     end
-  end
-
-  departaments_owner.uniq.each do |n|
-    home=Home.find(n)
-    puts  "#{ home.owner.user.name } #{ home.owner.user.last_name } #{ home.owner.user.email } #{ n } #{ home.total_amount } #{ home.status } #{ home.location } " 
-  end
+  
+    home_owner.uniq.each do |n|
+      home = Home.find(n)
+      puts  "#{ home.owner.user.name } #{ home.owner.user.last_name } #{ home.owner.user.email } #{ n } #{ home.total_amount } #{ home.status } #{ home.location } " 
+    end
+   end
+  
+  similar_homes
 end
-
-similar_homes
